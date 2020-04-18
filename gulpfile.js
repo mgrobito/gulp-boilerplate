6b4fc589defa096f08cd9e68fc82dab06360e12b
Santosh Magar <mgrsantox@gmail.com>
2020-04-18 21:54:10 +0545

    complete bootstrap and browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const clean = require('gulp-clean');
+const browserSync = require('browser-sync').create();
 
-const baseDir = 'dist';
+const baseDir = './';
+const distDir = 'dist';
 const srcSass = 'src/sass';
+const srcJs = 'src/sass';
 const distSass = 'dist/css';
+const distJs = 'dist/js';
 
 
+// Clean All the 'dist' Directory
 gulp.task('clean', function(){
-    return gulp.src(baseDir, {read: false})
+    return gulp.src(distDir, {read: false, allowEmpty: true})
         .pipe(clean({force: true}));
 });
 
@@ -18,8 +23,24 @@ gulp.task('style', function(){
         .pipe(sass().on('error', sass.logError))
         .pipe(sass({outputStyle: 'nested'}))
         .pipe(gulp.dest(distSass))
+        .pipe(browserSync.stream());
 });
 
 
+gulp.task('serve', function(){
+	browserSync.init({
+		server:{
+			baseDir:baseDir,
+			index: "/pages/index.html"
+		}
+	});
+	gulp.watch('src/sass/**/*.scss',gulp.series('style'));
+	gulp.watch('pages/*.html').on('change', browserSync.reload);
+	gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
+});
+
+
+gulp.task('reset', gulp.series('clean','style'));
+gulp.task('watch', gulp.parallel('serve'));
 
 gulp.task('default', gulp.parallel('style'));
